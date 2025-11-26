73ea240fe3d945463161c2fb3ec38113851c1e43
thevoxium <thevoxium@gmail.com>
2025-11-26 06:04:53 +0530

    some update

--- main.cpp
+++ main.cpp
@@ -1,36 +1,34 @@
+#include "src/engine.hpp"
+#include "src/loss.hpp"
 #include "src/tensor.hpp"
 #include "src/tensor_ops.hpp"
-#include "src/loss.hpp"
-#include "src/engine.hpp"
 #include <iostream>
-#include <cstdio>
 
 int main() {
-    Tensor* x = tensor_random(2, new int[2]{1000, 1});
-    Tensor* y = tensor_sin(x);
-   
-    Tensor* w = tensor_random(2, new int[2]{1, 1});
-    Tensor* b = tensor_random(2, new int[2]{1000, 1});
+  Tensor *x = tensor_random(2, new int[2]{1000, 1}, DEVICE_GPU);
+  Tensor *y = tensor_sin(x);
 
-    int epochs = 100;
-    for (int i=0 ; i< epochs; i++){
-        Tensor* z = tensor_mm(x, w);
-        Tensor* pred = tensor_add(z, b);
-        Tensor* loss = loss_mse(pred, y);
-        backward(loss);
-        w = tensor_sub(w, tensor_scale(w, 0.01f));
-        b = tensor_sub(b, tensor_scale(b, 0.01f));
-        std::cout << "Epoch " << i << " Loss: " << loss->data[0] << std::endl;
-        tensor_free(z);
-        tensor_free(pred);
-        tensor_free(loss);
-    }
+  Tensor *w = tensor_random(2, new int[2]{1, 1});
+  Tensor *b = tensor_random(2, new int[2]{1000, 1});
 
-    tensor_free(x);
-    tensor_free(y);
-    tensor_free(w);
-    tensor_free(b);
+  int epochs = 100;
+  for (int i = 0; i < epochs; i++) {
+    Tensor *z = tensor_mm(x, w);
+    Tensor *pred = tensor_add(z, b);
+    Tensor *loss = loss_mse(pred, y);
+    backward(loss);
+    w = tensor_sub(w, tensor_scale(w, 0.01f));
+    b = tensor_sub(b, tensor_scale(b, 0.01f));
+    std::cout << "Epoch " << i << " Loss: " << loss->data[0] << std::endl;
+    tensor_free(z);
+    tensor_free(pred);
+    tensor_free(loss);
+  }
 
-    return 0;
+  tensor_free(x);
+  tensor_free(y);
+  tensor_free(w);
+  tensor_free(b);
+
+  return 0;
 }
-
